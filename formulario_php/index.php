7bf03030d2501a9f802521eaa2559ffe0d00844f
eduardo1295 <lalalolo1221@gmail.com>
2020-06-04 17:48:13 -0600

    practica ya quedo

--- formulario_php/index.php
+++ formulario_php/index.php
@@ -7,33 +7,26 @@
     <script src="js/jquery-3.5.1.min.js"></script>
     <link rel="stylesheet" href="css/bootstrap.min.css">
     <script src="js/bootstrap.min.js"></script>
+    <script src="js/validacion.js"></script>
     <script src="js/script.js"></script>
     <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.21/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.2.5/css/responsive.dataTables.min.css">
+    <link rel="stylesheet" href="css/style.css">
+
     <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.js"></script>
+    <script src="https://cdn.datatables.net/1.10.21/js/jquery.dataTables.min.js "></script>
+    <script src="https://cdn.datatables.net/responsive/2.2.5/js/dataTables.responsive.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
+    <script src="https://kit.fontawesome.com/9417fd5572.js" crossorigin="anonymous"></script>
 </head>
 <body>
     <div class="container">
-        <h1 class="text-center">Registro de personas</h1>
-        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">Agregar</button>  
-
-        <table id="table_id" class="display">
-            <thead>
-                <tr>
-                    <th>Column 1</th>
-                    <th>Column 2</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Row 1 Data 1</td>
-                    <td>Row 1 Data 2</td>
-                </tr>
-                <tr>
-                    <td>Row 2 Data 1</td>
-                    <td>Row 2 Data 2</td>
-                </tr>
-            </tbody>
-        </table> 
+        <header>
+            <h1 class="text-center">Registro de personas</h1>
+            <button type="button" class="btn btn-primary btn-add" data-toggle="modal" data-target="#exampleModalCenter">Agregar</button>  
+        </header>
+        <?php include("tabla.php")?>    
     </div>
     <?php include("modal.php")?>
 </body>
